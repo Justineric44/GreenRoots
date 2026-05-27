aebacebd98cb7325622e525f3149f5e3404f1a67
Wafa Kebaier <231019759+wafatch-design@users.noreply.github.com>
2026-05-27 12:09:53 +0200

    feat: add helmet and cors security middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
+app.use(cors()); // Permet de gérer les requêtes cross-origin (CORS)
+app.use(helmet()); // Aide à sécuriser l'application en définissant divers en-têtes HTTP
 
 // Middlewares globaux
 app.use(express.json());
