5030bcc24819e47809fa1aac7e393dc9f6aba4a9
Justineric44 <justine.robard@hotmail.fr>
2026-05-27 11:56:06 +0200

    feat: add component navbar

---
+++ frontend/src/components/layout/navbar.tsx
@@ -0,0 +1,15 @@
+// import {
+//     InputGroup,
+//     InputGroupAddon,
+//     InputGroupButton,
+//     InputGroupInput,
+//     InputGroupText,
+//     InputGroupTextarea,
+//   } from "@/components/ui/input-group"
+
+//   <InputGroup>
+//   <InputGroupInput placeholder="Search..." />
+//   <InputGroupAddon>
+//     <SearchIcon />
+//   </InputGroupAddon>
+// </InputGroup>
