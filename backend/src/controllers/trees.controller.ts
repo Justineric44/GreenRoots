8c6cbc319f1a728722f55a14a562b4d51eef3282
Justineric44 <justine.robard@hotmail.fr>
2026-05-28 14:30:02 +0200

    feat: add backend contollers trees

--- backend/src/controllers/trees.controller.ts
+++ backend/src/controllers/trees.controller.ts
@@ -1,10 +1,46 @@
 import type { Request, Response } from 'express';
 import { prisma } from '../lib/prisma.js';
+// import z from "zod";
+// import { ConflictError, NotFoundError } from "../lib/errors.js";
+// import { parseIdFromParams } from "./utils.js";
 
+// ─────────────────────────────────────────────
+// GET /api/trees
+// ─────────────────────────────────────────────
 export async function getAllTrees(req: Request, res: Response) {
-  // Récupérer tous les arbres en BDD
-  const trees = await prisma.tree.findMany();
+  const page = Number(req.query.page) || 1;
+  const limit = 9;
+  const [trees, total] = await prisma.$transaction([
+    prisma.tree.findMany({
+      take: limit,
+      skip: limit * (page - 1),
+      select: {
+        id: true,
+        commonName: true,
+        family: true,
+        origin: true,
+        slug: true,
+        picture: true,
+      },
+    }),
+    prisma.tree.count(),
+  ]);
+  if (trees.length === 0) {
+    throw new NotFoundError();
+  }
+  res.json({ trees, total });
+}
 
-  // Les renvoyer
-  res.json(trees);
+// ─────────────────────────────────────────────
+// GET /api/trees/:slug
+// ─────────────────────────────────────────────
+export async function getOneTree(req: Request, res: Response) {
+  // On récupère le slug dans l'URL
+  // ex: GET /api/trees/pin-sylvestre
+  const { slug } = req.params;
+
+  const tree = await prisma.tree.findUnique({ where: { slug } });
+  if (!tree) throw new NotFoundError('Tree not found');
+
+  res.json(tree);
 }
