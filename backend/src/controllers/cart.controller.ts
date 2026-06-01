6734e25f3e0009d12d8a8654f2c1e68291e1fa65
CamilleDelmas <kamilledelmas@gmail.com>
2026-06-01 16:48:49 +0200

    feat: add comments on controller and begin the addItemToCart function

--- backend/src/controllers/cart.controller.ts
+++ backend/src/controllers/cart.controller.ts
@@ -1,11 +1,18 @@
 import type { Request, Response } from 'express';
-import { NotFoundError } from '../lib/errors.js';
+import { NotFoundError, ValidationError } from '../lib/errors.js';
 import { prisma } from '../lib/prisma.js';
+import { getOrCreateActiveCart } from '../lib/cartUtils.js';
 
 export async function getActiveCart(req: Request, res: Response) {
-  const userId = req.user?.userId;
-  const cart = await prisma.cart.findFirst({
-    where: { userId: userId!, status: 'active' },
+  // Récupère l'utilisateur courant depuis le token de session
+  const userId = Number(req.user?.userId);
+
+  // Crée un panier actif si besoin ou récupère celui existant
+  const cart = await getOrCreateActiveCart(userId);
+
+  // Inclut les détails des articles, des arbres et des projets associés
+  const cartWithItems = await prisma.cart.findUnique({
+    where: { id: cart.id },
     include: {
       items: {
         select: {
@@ -27,13 +34,46 @@ export async function getActiveCart(req: Request, res: Response) {
       },
     },
   });
-  if (!cart) {
+
+  if (!cartWithItems) {
     throw new NotFoundError();
   }
-  const cartTotal = cart.items.reduce(
+
+  // Calcule le total du panier en multipliant quantité par prix
+  const cartTotal = cartWithItems.items.reduce(
     (acc, item) => acc + item.quantity * Number(item.tree.price),
     0
   );
-  console.log(cartTotal);
-  res.json({ cart, cartTotal });
+
+  res.json({ cartWithItems, cartTotal });
+}
+
+export async function addItemToCart(req: Request) {
+  // Récupère l'utilisateur courant depuis le token de session
+  const userId = Number(req.user?.userId);
+
+  // Récupère les infos envoyés depuis le front
+  const { treeId, projectId, quantity } = req.body;
+
+  // Crée un panier actif si besoin ou récupère celui existant
+  const cart = await getOrCreateActiveCart(userId);
+  console.log(cart);
+
+  // Vérifie si l'arbre existe dans le projet
+  const treeAndProject = await prisma.projectHasTree.findUnique({
+    where: {
+      projectId_treeId: { projectId, treeId },
+    },
+  });
+  if (!treeAndProject) {
+    throw new NotFoundError('Arbre ou projet introuvable');
+  }
+
+  // Vérifie le stock de cet arbre pour ce projet
+  const stock = treeAndProject.stock;
+  if (quantity > stock) {
+    throw new ValidationError('Quantité non disponible');
+  }
+
+  // Vérifie si l'arbre est déjà dans le panier
 }
