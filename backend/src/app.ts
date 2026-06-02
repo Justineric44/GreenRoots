8c9457c8c3c2d6944e4f4a699e3494de36b592a0
Justineric44 <justine.robard@hotmail.fr>
2026-06-02 16:50:37 +0200

    feat: add feature search for mobile and desktop

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,22 +3,21 @@ import cors from 'cors';
 import helmet from 'helmet';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { router } from './routers/index.router.js';
+import searchRouter from './routers/searchRouter.js';
 
 const app = express();
-app.use(cors()); // Permet de gérer les requêtes cross-origin (CORS)
-app.use(helmet()); // Aide à sécuriser l'application en définissant divers en-têtes HTTP
 
-// Middlewares globaux
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
-// Permet de récupérer les données envoyées par les formulaires HTML (EJS) dans req.body.
 app.use(express.urlencoded({ extended: true }));
 
-// Route temporaire de test
 app.get('/', (_req, res) => {
   res.json({ message: 'GreenRoots API is running' });
 });
 
 app.use('/api', router);
+app.use('/api/search', searchRouter);
 
 app.use(errorHandler);
 
