d5d0ae843d6978652dc4043b6f24d8b361fcd08d
CamilleDelmas <kamilledelmas@gmail.com>
2026-06-04 10:46:41 +0200

    feat: add filtering and search options for projects

--- frontend/src/app/projets/page.tsx
+++ frontend/src/app/projets/page.tsx
@@ -1,6 +1,5 @@
 import Title from '@/components/layout/Title';
 import Image from 'next/image';
-import { notFound } from 'next/navigation';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import ProjectsPagination from '@/components/layout/ProjectsPagination';
@@ -15,9 +14,10 @@ import {
 import Link from 'next/link';
 import { Metadata } from 'next';
 import type { Project } from '@/types/index';
-import { getProjects } from '@/lib/api';
+import { getProjects, getProjectsLocalisations } from '@/lib/api';
 import { Progress } from '@/components/ui/progress';
 import { Field, FieldLabel } from '@/components/ui/field';
+import ProjectsFilters from '@/components/layout/ProjectsFilters';
 
 export const metadata: Metadata = {
   title: 'Projets de reforestation - GreenRoots',
@@ -28,22 +28,30 @@ export const metadata: Metadata = {
 export default async function ProjectsPage({
   searchParams,
 }: {
-  searchParams: { page?: string };
+  searchParams: Promise<{
+    page?: string;
+    localisation?: string;
+    search?: string;
+    sortBy?: string;
+    sortOrder?: string;
+  }>;
 }) {
   // Récupérer le numéro de page à partir des paramètres de recherche avec await pour s'assurer que les données sont disponibles avant de continuer
-  const { page } = await searchParams;
+  const { page, localisation, search, sortBy, sortOrder } = await searchParams;
   const currentPage = Number(page) || 1;
-  const { projects, total, limit } = await getProjects(currentPage);
-  if (!projects || projects.length === 0) {
-    return notFound();
-  }
-
+  const { projects, total, limit } = await getProjects(
+    currentPage,
+    localisation,
+    search,
+    sortBy,
+    sortOrder
+  );
+  const { localisations } = await getProjectsLocalisations();
   const totalPages = Math.ceil(total / limit);
-  console.log(totalPages);
   return (
     <main>
       <Title title="Nos projets" />
-      <section className="bg-brand-bg px-4 py-4 sm:px-4 lg:px-4 lg:py-4">
+      <section className="bg-brand-bg px-4 py-4 sm:px-4 lg:px-4 lg:py-2">
         <div className="mx-auto max-w-7xl p-8 text-brand-dark">
           <p className="pb-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
@@ -65,73 +73,78 @@ export default async function ProjectsPage({
           </p>
         </div>
       </section>
-      <section className="bg-brand-bg px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
-        <div className="mx-auto max-w-7xl p-8 text-brand-dark flex flex-row flex-wrap gap-4">
-          {projects.map((project: Project) => (
-            <Card
-              key={project.id}
-              className="relative mx-auto w-full max-w-sm pt-0"
-            >
-              <div className="absolute inset-0 z-30 aspect-video" />
-              <Badge
-                variant="secondary"
-                className="absolute top-2 right-2 z-40"
-              >
-                {project.localisation}
-              </Badge>
-              <Image
-                src={project.picture}
-                alt={project.name}
-                width={300}
-                height={200}
-                priority
-                className="relative z-20 aspect-video w-full object-cover"
+      <section className="bg-brand-bg px-4 pt-4 pb-16 sm:px-6 lg:px-8 lg:pt-4 lg:pb-20">
+        <div className="mx-auto max-w-7xl p-8 text-brand-dark">
+          <ProjectsFilters localisations={localisations} />
+
+          {!projects || projects.length === 0 ? (
+            <p className="text-center text-lg mt-10">
+              Aucun projet ne correspond à votre recherche.
+            </p>
+          ) : (
+            <>
+              <div className="flex flex-row flex-wrap gap-4 mb-8">
+                {projects.map((project: Project) => (
+                  <Card
+                    key={project.id}
+                    className="relative mx-auto w-full max-w-sm pt-0"
+                  >
+                    <div className="absolute inset-0 z-30 aspect-video" />
+                    <Badge
+                      variant="secondary"
+                      className="absolute top-2 right-2 z-40"
+                    >
+                      {project.localisation}
+                    </Badge>
+                    <Image
+                      src={project.picture}
+                      alt={project.name}
+                      width={300}
+                      height={200}
+                      priority
+                      className="relative z-20 aspect-video w-full object-cover"
+                    />
+
+                    <CardHeader>
+                      <CardTitle>{project.name}</CardTitle>
+                      <CardDescription className="min-h-[3rem]">
+                        {project.shortDescription}
+                      </CardDescription>
+
+                      <Field className="w-full max-w-sm">
+                        <FieldLabel htmlFor="progress-upload">
+                          <span>Progression</span>
+                          <span className="ml-auto">{project.progress}%</span>
+                        </FieldLabel>
+                        <Progress
+                          value={project.progress}
+                          id="progress-upload"
+                          className="w-full"
+                        />
+                      </Field>
+                    </CardHeader>
+                    <CardFooter>
+                      <Button className="w-full bg-accent">
+                        <Link
+                          href={`/projets/${project.slug}`}
+                          className="w-full"
+                        >
+                          Voir le projet
+                        </Link>
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                ))}
+              </div>
+
+              <ProjectsPagination
+                currentPage={currentPage}
+                totalPages={totalPages}
               />
-
-              <CardHeader>
-                <CardTitle>{project.name}</CardTitle>
-                <CardDescription className="min-h-[3rem]">
-                  {project.shortDescription}
-                </CardDescription>
-
-                <Field className="w-full max-w-sm">
-                  <FieldLabel htmlFor="progress-upload">
-                    <span>Progression</span>
-                    <span className="ml-auto">66%</span>
-                  </FieldLabel>
-                  <Progress
-                    value={project.progress}
-                    id="progress-upload"
-                    className="w-full"
-                  />
-                </Field>
-              </CardHeader>
-              <CardFooter>
-                <Button className="w-full bg-accent">
-                  <Link href={`/projets/${project.slug}`} className="w-full">
-                    Voir le projet
-                  </Link>
-                </Button>
-              </CardFooter>
-            </Card>
-          ))}
-
-          <ProjectsPagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-          />
+            </>
+          )}
         </div>
       </section>
     </main>
   );
 }
-
-//  <Image
-//                       src="/images/projects/details/tree-progress.svg"
-//                       alt="Progression"
-//                       width={40}
-//                       height={40}
-//                       className="absolute -top-2 -translate-x-1/2"
-//                       style={{ left: `${progress}%` }}
-//                     />
-//                   </div>
